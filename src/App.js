1ee9ea3af3597a7e04029573df95063298bb0995
Stephen Wang <stephenwang@SteveBook.local>
2025-04-13 21:02:19 -0400

    04-13-2025: Added Vercel Analytics

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import {BrowserRouter as Router, Route, Routes} from 'react-router-dom';
+import { Analytics } from '@vercel/analytics/react';
 import Home from './pages/Home';
 
 
@@ -9,6 +10,7 @@ function App() {
       <Routes>
         <Route exact path = "/" element = {<Home />} />
       </Routes>
+      <Analytics />
     </Router>
   );
 }
